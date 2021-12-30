2d88120bf4c6fc7bcfb028616c616e660cc2240b
Alesal2021 <alessandrocontato1975@gmail.com>
2021-12-30 01:21:58 -0300

    updade

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -5,10 +5,46 @@ import br.com.dio.model.gato;
 public class primeiroprograma {
     public static void main(String[] args) {
         gato gato = new gato();
-
         System.out.println(gato);
+
+
+
+
        /* int a = 5;
         int  b = 6;
         System.out.println("hello world " + (a+b));*/
+    }
+}
+class livro{
+    private String nome;
+    private Integer numpagina;
+
+    public livro(String nome, Integer numpagina) {
+        this.nome = nome;
+        this.numpagina = numpagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpagina() {
+        return numpagina;
+    }
+
+    public void setNumpagina(Integer numpagina) {
+        this.numpagina = numpagina;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numpagina=" + numpagina +
+                '}';
     }
 }
